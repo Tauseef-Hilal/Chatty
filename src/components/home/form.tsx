19ec30a367f438c76772a4db52aca7bf8c8165c3
Tauseef-Hilal <tantary.tauseef.atwork@gmail.com>
2023-05-07 11:59:56 +0530

    Complete Home UI (almost)

---
+++ src/components/home/form.tsx
@@ -0,0 +1,58 @@
+import styles from "./form.module.css";
+
+type FormButtonData = {
+  btnValue: string;
+  btnHandler: () => void;
+};
+
+type FormProps = {
+  formLabel: string;
+  inputValue: string;
+  inputPlaceholder: string;
+  inputFieldClass: string;
+  inputChangeHandler: (value: string) => void;
+  formButtonData: FormButtonData[];
+};
+
+export default function CustomForm({
+  formLabel,
+  inputValue,
+  inputPlaceholder,
+  inputFieldClass,
+  inputChangeHandler,
+  formButtonData,
+}: FormProps) {
+  return (
+    <div className={styles.form}>
+      <div className={styles.formContent}>
+        <label className={styles.formFieldLabel} htmlFor="formField">
+          {formLabel}
+        </label>
+
+        <input
+          onChange={(e) => inputChangeHandler(e.target.value)}
+          value={inputValue}
+          className={`${styles.formField} ${inputFieldClass}`}
+          type="text"
+          id="formField"
+          placeholder={inputPlaceholder}
+        />
+      </div>
+
+      <div className={styles.formBtns}>
+        {formButtonData.map((btnData, idx) => {
+          return (
+            <button
+              key={idx}
+              onClick={btnData.btnHandler}
+              className={styles.formBtn}
+              type="button"
+            >
+              {btnData.btnValue}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,44 @@
 import Head from "next/head";
+import styles from "@/styles/home.module.css";
+import CustomForm from "@/components/home/form";
+import { useState } from "react";
+
+enum FormType {
+  INITIAL,
+  JOIN_ROOM_FORM,
+  CREATE_ROOM_FORM,
+}
 
 export default function Home() {
+  const [username, setUsername] = useState("");
+  const [roomId, setRoomId] = useState("");
+  const [roomName, setRoomName] = useState("");
+  const [formType, setFormType] = useState(FormType.INITIAL);
+  const [inputFieldClass, setInputFieldClass] = useState("");
+
+  function handleUsernameChange(newUsername: string) {
+    validateName(newUsername);
+    setUsername(newUsername);
+  }
+
+  function handleRoomIdChange(newRoomId: string) {
+    setRoomId(newRoomId);
+  }
+
+  function handleRoomNameChange(newRoomName: string) {
+    validateName(newRoomName);
+    setRoomName(newRoomName);
+  }
+
+  function validateName(name: string) {
+    if (name.length < 3) {
+      setInputFieldClass(styles.invalid);
+      return;
+    }
+
+    setInputFieldClass(styles.valid);
+  }
+
   return (
     <>
       <Head>
@@ -9,7 +47,63 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>Hello World</main>
+      <main className={styles.main}>
+        <h1 className={styles.heading}>
+          Welcome To <span className={styles.chatty}>Chatty</span>
+        </h1>
+
+        {formType == FormType.INITIAL && (
+          <CustomForm
+            formLabel="Enter Username"
+            inputValue={username}
+            inputPlaceholder="Eg. strange_guy"
+            inputFieldClass={inputFieldClass}
+            inputChangeHandler={handleUsernameChange}
+            formButtonData={[
+              {
+                btnValue: "JOIN ROOM",
+                btnHandler: () => setFormType(FormType.JOIN_ROOM_FORM),
+              },
+              {
+                btnValue: "CREATE NEW ROOM",
+                btnHandler: () => setFormType(FormType.CREATE_ROOM_FORM),
+              },
+            ]}
+          />
+        )}
+
+        {formType == FormType.JOIN_ROOM_FORM && (
+          <CustomForm
+            formLabel="Enter Room ID"
+            inputValue={roomId}
+            inputPlaceholder="Eg. 12345678"
+            inputFieldClass={inputFieldClass}
+            inputChangeHandler={handleRoomIdChange}
+            formButtonData={[
+              {
+                btnValue: "JOIN ROOM",
+                btnHandler: () => "",
+              },
+            ]}
+          />
+        )}
+
+        {formType == FormType.CREATE_ROOM_FORM && (
+          <CustomForm
+            formLabel="Enter Room Name"
+            inputValue={roomName}
+            inputPlaceholder="Eg. strange_guys"
+            inputFieldClass={inputFieldClass}
+            inputChangeHandler={handleRoomNameChange}
+            formButtonData={[
+              {
+                btnValue: "CREATE ROOM",
+                btnHandler: () => "",
+              },
+            ]}
+          />
+        )}
+      </main>
     </>
   );
 }
